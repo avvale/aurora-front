9effa0191c6c60adb8818d3be18eeb265a775e89
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-06-20 19:48:45 +0200

    init

---
+++ src/@aurora/aurora.module.ts
@@ -0,0 +1,36 @@
+import { APP_INITIALIZER, NgModule, Optional, SkipSelf } from '@angular/core';
+import { ApolloModule } from './modules';
+import { BootstrapService } from './services/bootstrap.service';
+import { PipesModule } from './pipes';
+import './prototypes/string-to-pascal-case.interface';
+import './prototypes/string-to-pascal-case';
+
+@NgModule({
+    imports: [
+        ApolloModule,
+        PipesModule,
+    ],
+    providers: [
+        {
+            provide   : APP_INITIALIZER,
+            useFactory: (bootstrapService: BootstrapService): () => void => () => bootstrapService.init(),
+            deps      : [BootstrapService],
+            multi     : true,
+        },
+    ],
+})
+export class AuroraModule
+{
+    /**
+     * Constructor
+     */
+    constructor(
+        @Optional() @SkipSelf() parentModule?: AuroraModule,
+    )
+    {
+        if ( parentModule )
+        {
+            throw new Error('AuroraModule has already been loaded. Import this module in the AppModule only!');
+        }
+    }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true,
+    debug     : false,
+    api       : {
+        graphql: 'http://localhost:8080/graphql',
+        rest   : 'http://localhost:8080',
+    },
+    lang: {
+        base    : 'es',
+        fallback: 'es',
+        langs   : ['es','en'],
+    },
+    oAuth: {
+        applicationCode  : '',
+        applicationSecret: '',
+    },
+};
