922ceb3af7c14bd192ceda82f726561dd95ae102
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-07-03 23:16:31 +0200

    minor fix

--- src/@aurora/components/select-element-grid/select-element-grid.module.ts
+++ src/@aurora/components/select-element-grid/select-element-grid.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { SelectElementGridComponent } from './select-element-grid.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
 import { GridDialogModule } from '../grid-dialog';
 
 @NgModule({
     imports: [
-        MatButtonModule,
-        MatDialogModule,
-        MatIconModule,
-
         // @aurora
         GridDialogModule,
     ],
