212beaa36fca53e3e5576a6df7b106ab8481c2d0
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2022-07-30 23:39:37 +0200

    upgrade @fuse v14.2.0 -> v15.1.0

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { IamService } from '@aurora';
 import { combineLatest, filter, map, Subject, takeUntil } from 'rxjs';
 import { FuseConfigService } from '@fuse/services/config';
+import { FusePlatformService } from '@fuse/services/platform';
 import { FuseMediaWatcherService } from '@fuse/services/media-watcher';
 import { FUSE_VERSION } from '@fuse/version';
 import { Layout } from 'app/layout/layout.types';
@@ -33,6 +34,7 @@ export class LayoutComponent implements OnInit, OnDestroy
         private _router: Router,
         private _fuseConfigService: FuseConfigService,
         private _fuseMediaWatcherService: FuseMediaWatcherService,
+        private _fusePlatformService: FusePlatformService,
         private iamService: IamService,
     )
     {
@@ -110,6 +112,9 @@ export class LayoutComponent implements OnInit, OnDestroy
 
         // Set the app version
         this._renderer2.setAttribute(this._document.querySelector('[ng-version]'), 'fuse-version', FUSE_VERSION);
+
+        // Set the OS name
+        this._renderer2.addClass(this._document.body, this._fusePlatformService.osName);
     }
 
     /**
