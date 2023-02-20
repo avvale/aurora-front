e88a70c0336382153d521069472605de4d244953
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-02-20 23:12:37 +0100

    wip

--- src/app/modules/admin/admin.navigation.ts
+++ src/app/modules/admin/admin.navigation.ts
@@ -1,11 +1,9 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
-import { exampleNavigation } from './example/example.navigation';
 import { oAuthNavigation } from './apps/o-auth/o-auth.navigation';
 import { iamNavigation } from './apps/iam/iam.navigation';
 import { auditingNavigation } from './apps/auditing/auditing.navigation';
 
 export const adminNavigation: FuseNavigationItem[] = [
-    exampleNavigation,
     oAuthNavigation,
     iamNavigation,
     auditingNavigation,
