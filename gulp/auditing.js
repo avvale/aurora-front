03bafaa7d4eac276be0f6c3c914cf96b82537ca7
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-02-23 18:37:59 +0100

    publish auditing

--- gulp/auditing.js
+++ gulp/auditing.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('node:fs');
@@ -11,17 +12,17 @@ function copyAuditing()
 {
     return Promise.all([
         fse.copy('cliter/auditing', 'publish/cliter/auditing', { overwrite: true }),
-        fse.copy('src/@api/auditing', 'publish/src/@api/auditing', { overwrite: true }),
-        fse.copy('src/@app/auditing', 'publish/src/@app/auditing', { overwrite: true }),
+        fse.copy('src/app/modules/admin/apps/auditing', 'publish/src/app/modules/admin/apps/auditing', { overwrite: true }),
+        fse.copy('src/assets/i18n/auditing', 'publish/src/assets/i18n/auditing', { overwrite: true }),
     ]);
 }
 
 function copyToCLI()
 {
     // remove old cli auditing files
-    fs.rmSync('../aurora-cli/src/templates/back/packages/auditing', { recursive: true, force: true });
+    fs.rmSync('../aurora-cli/src/templates/front/packages/auditing', { recursive: true, force: true });
     // copy new cli auditing files
-    return fse.copy('publish', '../aurora-cli/src/templates/back/packages/auditing', { overwrite: true });
+    return fse.copy('publish', '../aurora-cli/src/templates/front/packages/auditing', { overwrite: true });
 }
 
 async function clean()
