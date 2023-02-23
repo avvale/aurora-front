c0955f15d17e11bfe9d9fbf78f4a69e2fa686616
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-02-23 10:42:26 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,14 @@ import { ExtraOptions, PreloadAllModules, RouteReuseStrategy, RouterModule } fro
 import { FuseModule } from '@fuse';
 import { FuseConfigModule } from '@fuse/services/config';
 import { FuseMockApiModule } from '@fuse/lib/mock-api';
-import { AuroraModule, AuthenticationService, AuthenticationMockAdapterService, EnvironmentsInformationService, EnvironmentsInformationMockAdapterService, AuthenticationDisabledAdapterGuard, LangService, JsonLangService, RouteReuseStrategyService, UserMetaStorageService, SessionService, SessionLocalStorageService, IamService, GridManagerService, AuroraGridManagerService, IamMockAdapterService, AuthenticationAuroraAdapterService } from '@aurora';
-import { HORIZONTAL_NAVIGATION, FUTURISTIC_NAVIGATION, DEFAULT_NAVIGATION, COMPACT_NAVIGATION } from '@aurora/components/navigation/navigation.types';
-import { CoreModule } from 'app/core/core.module';
-import { appConfig } from 'app/core/config/app.config';
+import { AuroraGridManagerService, AuroraModule, AuthenticationAuroraAdapterService, AuthenticationDisabledAdapterGuard, AuthenticationService, COMPACT_NAVIGATION, DEFAULT_NAVIGATION, EnvironmentsInformationMockAdapterService, EnvironmentsInformationService, FUTURISTIC_NAVIGATION, GridManagerService, HORIZONTAL_NAVIGATION, IamAuroraAdapterService, IamService, JsonLangService, LangService, RibbonEnvironmentModule, RouteReuseStrategyService, SessionLocalStorageService, SessionService, UserMetaStorageLocalStorageAdapterService, UserMetaStorageService } from '@aurora';
+import { appConfig, AuthGuard, CoreModule } from 'app/core';
 import { mockApiServices } from 'app/mock-api';
 import { LayoutModule } from 'app/layout/layout.module';
 import { AppComponent } from 'app/app.component';
 import { appRoutes } from 'app/app.routing';
-import { UserMetaStorageLocalStorageService } from '@aurora/components/user-meta-storage/user-meta-storage-local-storage-adapter.service';
+import { UserMetaStorageIamAdapterService } from './modules/admin/apps/iam/user-meta/user-meta-storage-iam-adapter.service';
 import { compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation } from './core/navigation/default-navigation';
-import { AuthGuard } from './core/auth/guards/auth.guard';
 
 const routerConfig: ExtraOptions = {
     preloadingStrategy       : PreloadAllModules,
@@ -44,6 +41,7 @@ const routerConfig: ExtraOptions = {
 
         // Aurora front module
         AuroraModule,
+        RibbonEnvironmentModule,
     ],
     providers: [
         {
@@ -89,11 +87,6 @@ const routerConfig: ExtraOptions = {
         {
             provide : HORIZONTAL_NAVIGATION,
             useValue: horizontalNavigation,
-        },
-        // disable authentication Guard
-        {
-            provide : AuthGuard,
-            useClass: AuthenticationDisabledAdapterGuard,
         },
     ],
     bootstrap: [
