1eaf457233c823a7b832154f8878d638a2da8141
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-03-13 22:34:17 +0100

    gulp application

--- gulp/application.js
+++ gulp/application.js
@@ -36,10 +36,12 @@ function copyApplication()
             '!src/app/modules/admin/apps/auditing/**',
             '!src/app/modules/admin/apps/iam/**',
             '!src/app/modules/admin/apps/o-auth/**',
+            '!src/app/modules/admin/apps/queue-manager/**',
             '!src/app/modules/azure-ad/**',
             '!src/assets/i18n/auditing/**',
             '!src/assets/i18n/iam/**',
             '!src/assets/i18n/o-auth/**',
+            '!src/assets/i18n/queue-manager/**',
             '!src/index.ts',
             '!gulpfile.js',
             '!package.json',
@@ -62,8 +64,9 @@ function editPackageJson()
         .pipe(
             jeditor(function(json)
             {
-                delete json.devDependencies['@azure/msal-angular'];
-                delete json.devDependencies['@azure/msal-browser'];
+                delete json.dependencies['@azure/msal-angular'];
+                delete json.dependencies['@azure/msal-browser'];
+
                 delete json.devDependencies['fs-extra'];
                 delete json.devDependencies['gulp'];
                 delete json.devDependencies['gulp-json-editor'];
@@ -128,13 +131,63 @@ async function cleanAppModule()
 
     // remove UserMetaStorageIamService
     codeWriter.removeImport(sourceFile, './modules/admin/apps/iam/user-meta/user-meta-storage-iam-adapter.service');
-    codeWriter.changeDecoratorPropertyAdapter(sourceFile, 'AppModule', 'providers', 'UserMetaStorageService', 'UserMetaStorageLocalStorageAdapterService');
+    codeWriter.changeDecoratorPropertyAdapter(
+        sourceFile,
+        'AppModule',
+        'providers',
+        'UserMetaStorageService',
+        'UserMetaStorageLocalStorageAdapterService',
+    );
+
+    // remove AzureModule
+    codeWriter.removeImport(sourceFile, './modules/azure-ad/azure-ad.module');
+    codeWriter.removeDecoratorProperty(
+        sourceFile,
+        'AppModule',
+        'NgModule',
+        'imports',
+        'AzureAdModule',
+    );
+
+    // remove MsalGuard
+    codeWriter.removeImport(sourceFile, '@azure/msal-angular');
+    codeWriter.removeDecoratorPropertyAdapter(
+        sourceFile,
+        'AppModule',
+        'NgModule',
+        'providers',
+        'AuthGuard',
+    );
+
+    // remove AuthorizationService
+    codeWriter.removeImport(sourceFile, './modules/azure-ad/authorization-azure-ad-adapter.service');
+    codeWriter.removeDecoratorPropertyAdapter(
+        sourceFile,
+        'AppModule',
+        'NgModule',
+        'providers',
+        'AuthorizationService',
+    );
 
     // change AuthenticationService
-    codeWriter.changeDecoratorPropertyAdapter(sourceFile, 'AppModule', 'providers', 'AuthenticationService', 'AuthenticationMockAdapterService');
+    codeWriter.removeImport(sourceFile, './modules/azure-ad/authentication-azure-ad-adapter.service');
+    codeWriter.changeDecoratorPropertyAdapter(
+        sourceFile,
+        'AppModule',
+        'providers',
+        'AuthenticationService',
+        'AuthenticationMockAdapterService',
+    );
 
     // change IamService
-    codeWriter.changeDecoratorPropertyAdapter(sourceFile, 'AppModule', 'providers', 'IamService', 'IamMockAdapterService');
+    codeWriter.removeImport(sourceFile, './modules/azure-ad/iam-azure-ad-adapter.service');
+    codeWriter.changeDecoratorPropertyAdapter(
+        sourceFile,
+        'AppModule',
+        'providers',
+        'IamService',
+        'IamMockAdapterService',
+    );
 
     // add EnvironmentsInformationMockAdapterService implementation
     codeWriter.addDecoratorPropertyAdapter(
@@ -163,6 +216,27 @@ async function cleanAppModule()
     sourceFile.saveSync();
 }
 
+async function cleanEnvironments()
+{
+    const project = codeWriter.createProject(['publish', 'tsconfig.json']);
+
+    const environmentFile = codeWriter.createSourceFile(project, ['publish', 'src', 'environments', 'environment.ts']);
+    codeWriter.removeObjectProperty(environmentFile, 'environment', 'azureAd');
+    environmentFile.saveSync();
+
+    const environmentProdFile = codeWriter.createSourceFile(project, ['publish', 'src', 'environments', 'environment.prod.ts']);
+    codeWriter.removeObjectProperty(environmentProdFile, 'environment', 'azureAd');
+    environmentProdFile.saveSync();
+
+    const environmentLocalFile = codeWriter.createSourceFile(project, ['publish', 'src', 'environments', 'environment.local.ts']);
+    codeWriter.removeObjectProperty(environmentLocalFile, 'environment', 'azureAd');
+    environmentLocalFile.saveSync();
+
+    const environmentDevFile = codeWriter.createSourceFile(project, ['publish', 'src', 'environments', 'environment.dev.ts']);
+    codeWriter.removeObjectProperty(environmentDevFile, 'environment', 'azureAd');
+    environmentDevFile.saveSync();
+}
+
 function copyToCLI()
 {
     // remove old cli application files
@@ -184,6 +258,7 @@ exports.publishApplication = series(
     cleanAppRouting,
     cleanAdminNavigation,
     cleanAppModule,
+    cleanEnvironments,
     copyToCLI,
     clean,
 );
