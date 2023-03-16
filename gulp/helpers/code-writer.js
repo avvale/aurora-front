2d864e733f914dccc95e3c23393b58328ecc3565
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-03-16 17:08:49 +0100

    wip

--- gulp/helpers/code-writer.js
+++ gulp/helpers/code-writer.js
@@ -5,56 +5,36 @@ const ts = require('typescript');
 const path = require('node:path');
 const tsMorph = require('ts-morph');
 
-exports.createProject = tsconfigPath =>
+exports.addArrayItem = (arrayToManage, item, finder) =>
 {
-    return new tsMorph.Project({
-        tsConfigFilePath    : path.join(process.cwd(), ...tsconfigPath),
-        // these are the defaults
-        manipulationSettings: {
-            // TwoSpaces, FourSpaces, EightSpaces, or Tab
-            indentationText                : tsMorph.IndentationText.FourSpaces,
-            // LineFeed or CarriageReturnLineFeed
-            newLineKind                    : ts.NewLineKind.LineFeed,
-            // Single or Double
-            quoteKind                      : tsMorph.QuoteKind.Single,
-            // Whether to change shorthand property assignments to property assignments
-            // and add aliases to import & export specifiers (see more information in
-            // the renaming section of the documentation).
-            usePrefixAndSuffixTextForRename: false,
-            // Whether to use trailing commas in multi-line scenarios where trailing
-            // commas would be used.
-            useTrailingCommas              : false,
-        },
-    });
-};
-
-exports.createSourceFile = (project, filePath) =>
-{
-    return project.addSourceFileAtPath(path.join(process.cwd(), ...filePath));
-};
-
-exports.removeImport = (sourceFile, importPath) =>
-{
-    const someModuleImport = sourceFile.getImportDeclaration(importPath);
-    someModuleImport.remove();
-};
-
-exports.removeDecoratorProperty = (sourceFile, moduleName, decoratorNameName, propertyName, valueName) =>
-{
-    const moduleClass = sourceFile.getClass(moduleName);
-    const moduleDecorator = moduleClass.getDecorator(decoratorNameName);
-    const moduleDecoratorArguments = moduleDecorator.getArguments()[0];
-    const importsArgument = moduleDecoratorArguments.getProperty(propertyName);
-    const importsArray = importsArgument.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression);
-
-    for (const [index, value] of importsArray.getElements().entries())
+    if (!this.isDuplicateArrayValue(arrayToManage, item, finder))
     {
-        if (value.getText() === valueName)
+        arrayToManage?.addElement(item, { useNewLines: true });
+    }
+};
+
+exports.addCallExpressionArgument = (sourceFile, functionName, argumentName) =>
+{
+    const callExpressions = sourceFile.getDescendantsOfKind(tsMorph.SyntaxKind.CallExpression);
+
+    for (const callExpression of callExpressions)
+    {
+        if (callExpression.getExpression().getText() === functionName)
         {
-            importsArray.removeElement(index);
+            callExpression.addArgument(argumentName);
             break;
         }
     }
+};
+
+exports.addDecoratorPropertyAdapter = (sourceFile, moduleName, propertyName, item) =>
+{
+    const moduleClass = sourceFile.getClass(moduleName);
+    const moduleDecorator = moduleClass.getDecorator('NgModule');
+    const moduleDecoratorArguments = moduleDecorator.getArguments()[0];
+    const decoratorProperty = moduleDecoratorArguments.getProperty(propertyName);
+    const decoratorArrayProperty = decoratorProperty.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression);
+    this.addArrayItem(decoratorArrayProperty, item);
 };
 
 exports.changeDecoratorPropertyAdapter = (sourceFile, moduleName, propertyName, provide, adapter) =>
@@ -88,6 +68,118 @@ exports.changeDecoratorPropertyAdapter = (sourceFile, moduleName, propertyName, 
     }
 };
 
+exports.createProject = tsconfigPath =>
+{
+    return new tsMorph.Project({
+        tsConfigFilePath    : path.join(process.cwd(), ...tsconfigPath),
+        // these are the defaults
+        manipulationSettings: {
+            // TwoSpaces, FourSpaces, EightSpaces, or Tab
+            indentationText                : tsMorph.IndentationText.FourSpaces,
+            // LineFeed or CarriageReturnLineFeed
+            newLineKind                    : ts.NewLineKind.LineFeed,
+            // Single or Double
+            quoteKind                      : tsMorph.QuoteKind.Single,
+            // Whether to change shorthand property assignments to property assignments
+            // and add aliases to import & export specifiers (see more information in
+            // the renaming section of the documentation).
+            usePrefixAndSuffixTextForRename: false,
+            // Whether to use trailing commas in multi-line scenarios where trailing
+            // commas would be used.
+            useTrailingCommas              : false,
+        },
+    });
+};
+
+exports.createSourceFile = (project, filePath) =>
+{
+    return project.addSourceFileAtPath(path.join(process.cwd(), ...filePath));
+};
+
+exports.getImportedDeclarations = sourceFile =>
+{
+    const imports = sourceFile.getImportDeclarations();
+    let declarations = [];
+    for (const importObj of imports)
+    {
+        declarations = [...declarations, ...importObj.getNamedImports().map(i => i.getName())];
+    }
+
+    return declarations;
+};
+
+exports.hasImportDecorations = (sourceFile, decoratorName) =>
+{
+    const importedDeclarations = this.getImportedDeclarations(sourceFile);
+
+    return importedDeclarations.includes(decoratorName);
+};
+
+exports.isDuplicateArrayValue = (arrayToManage, item, finder) =>
+{
+    // format string to avoid break spaces and extra white spaces
+    const arrayItems = arrayToManage?.getElements().map(i => i.getText()).map(j => j.replace(/(\r\n|\n|\r|\s)/gm, ''));
+
+    if (finder) return finder(item, arrayToManage);
+
+    if (Array.isArray(arrayItems)) return arrayItems.includes(item.replace(/(\r\n|\n|\r|\s)/gm, ''));
+
+    return false;
+};
+
+exports.removeArrayItemsAccordValue = (arrayToManage, valuesToDelete) =>
+{
+    for (const [index, value] of arrayToManage.getElements().entries())
+    {
+        const valueName = value.getText();
+
+        if (valuesToDelete.includes(valueName.replaceAll('\'', '')))
+        {
+            arrayToManage.removeElement(index);
+            this.removeArrayItemsAccordValue(arrayToManage, valuesToDelete);
+            break;
+        }
+    }
+};
+
+exports.removeCallExpressionArgument = (sourceFile, functionName, argumentName) =>
+{
+    const callExpressions = sourceFile.getDescendantsOfKind(tsMorph.SyntaxKind.CallExpression);
+
+    for (const callExpression of callExpressions)
+    {
+        if (callExpression.getExpression().getText() === functionName)
+        {
+            for (const [index, value] of callExpression.getArguments().entries())
+            {
+                if (value.getText() === argumentName)
+                {
+                    callExpression.removeArgument(index);
+                    break;
+                }
+            }
+        }
+    }
+};
+
+exports.removeDecoratorProperty = (sourceFile, moduleName, decoratorNameName, propertyName, valueName) =>
+{
+    const moduleClass = sourceFile.getClass(moduleName);
+    const moduleDecorator = moduleClass.getDecorator(decoratorNameName);
+    const moduleDecoratorArguments = moduleDecorator.getArguments()[0];
+    const importsArgument = moduleDecoratorArguments.getProperty(propertyName);
+    const importsArray = importsArgument.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression);
+
+    for (const [index, value] of importsArray.getElements().entries())
+    {
+        if (value.getText() === valueName)
+        {
+            importsArray.removeElement(index);
+            break;
+        }
+    }
+};
+
 exports.removeDecoratorPropertyAdapter = (sourceFile, moduleName, decoratorName, propertyName, provide) =>
 {
     const moduleClass = sourceFile.getClass(moduleName);
@@ -114,6 +206,12 @@ exports.removeDecoratorPropertyAdapter = (sourceFile, moduleName, decoratorName,
     }
 };
 
+exports.removeImport = (sourceFile, importPath) =>
+{
+    const someModuleImport = sourceFile.getImportDeclaration(importPath);
+    if (someModuleImport) someModuleImport.remove();
+};
+
 exports.removeItemsFromObjectArrayAccordPropertyValue = (arrayToManage, propertyName, valuesToDelete) =>
 {
     for (const [index, value] of arrayToManage.getElements().entries())
@@ -131,89 +229,10 @@ exports.removeItemsFromObjectArrayAccordPropertyValue = (arrayToManage, property
     }
 };
 
-exports.removeArrayItemsAccordValue = (arrayToManage, valuesToDelete) =>
-{
-    for (const [index, value] of arrayToManage.getElements().entries())
-    {
-        const valueName = value.getText();
-
-        if (valuesToDelete.includes(valueName.replaceAll('\'', '')))
-        {
-            arrayToManage.removeElement(index);
-            this.removeArrayItemsAccordValue(arrayToManage, valuesToDelete);
-            break;
-        }
-    }
-};
-
-exports.addDecoratorPropertyAdapter = (sourceFile, moduleName, propertyName, item) =>
-{
-    const moduleClass = sourceFile.getClass(moduleName);
-    const moduleDecorator = moduleClass.getDecorator('NgModule');
-    const moduleDecoratorArguments = moduleDecorator.getArguments()[0];
-    const decoratorProperty = moduleDecoratorArguments.getProperty(propertyName);
-    const decoratorArrayProperty = decoratorProperty.getInitializerIfKindOrThrow(ts.SyntaxKind.ArrayLiteralExpression);
-    this.addArrayItem(decoratorArrayProperty, item);
-};
-
-exports.addArrayItem = (arrayToManage, item, finder) =>
-{
-    if (!this.isDuplicateArrayValue(arrayToManage, item, finder))
-    {
-        arrayToManage?.addElement(item, { useNewLines: true });
-    }
-};
-
-exports.isDuplicateArrayValue = (arrayToManage, item, finder) =>
-{
-    // format string to avoid break spaces and extra white spaces
-    const arrayItems = arrayToManage?.getElements().map(i => i.getText()).map(j => j.replace(/(\r\n|\n|\r|\s)/gm, ''));
-
-    if (finder) return finder(item, arrayToManage);
-
-    if (Array.isArray(arrayItems)) return arrayItems.includes(item.replace(/(\r\n|\n|\r|\s)/gm, ''));
-
-    return false;
-};
-
 exports.removeObjectProperty = (sourceFile, variableInitializer, propertyName) =>
 {
     const variable = sourceFile.getVariableDeclarationOrThrow(variableInitializer);
     const obj = variable.getInitializerIfKindOrThrow(ts.SyntaxKind.ObjectLiteralExpression);
     const property = obj.getProperty(propertyName);
     if (property) property.remove();
-};
-
-exports.removeCallExpressionArgument = (sourceFile, functionName, argumentName) =>
-{
-    const callExpressions = sourceFile.getDescendantsOfKind(tsMorph.SyntaxKind.CallExpression);
-
-    for (const callExpression of callExpressions)
-    {
-        if (callExpression.getExpression().getText() === functionName)
-        {
-            for (const [index, value] of callExpression.getArguments().entries())
-            {
-                if (value.getText() === argumentName)
-                {
-                    callExpression.removeArgument(index);
-                    break;
-                }
-            }
-        }
-    }
-};
-
-exports.addCallExpressionArgument = (sourceFile, functionName, argumentName) =>
-{
-    const callExpressions = sourceFile.getDescendantsOfKind(tsMorph.SyntaxKind.CallExpression);
-
-    for (const callExpression of callExpressions)
-    {
-        if (callExpression.getExpression().getText() === functionName)
-        {
-            callExpression.addArgument(argumentName);
-            break;
-        }
-    }
 };
