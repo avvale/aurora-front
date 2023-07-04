20a0488caf9328ee0e78d86eac9c55b7d6674978
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-04 19:02:36 +0200

    update app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -108,6 +108,10 @@ export const appConfig: ApplicationConfig = {
             useClass: SessionLocalStorageService,
         },
         {
+            provide : IamService,
+            useClass: IamAuroraAdapterService,
+        },
+        {
             provide : GridManagerService,
             useClass: AuroraGridManagerService,
         },
