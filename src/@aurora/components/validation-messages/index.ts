c30152a6684ffdcce5856934d7c256dc9ab10a94
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-07 22:45:58 +0200

    wip

--- src/@aurora/components/validation-messages/index.ts
+++ src/@aurora/components/validation-messages/index.ts
@@ -1,4 +1,1 @@
-export { translocoLoader } from './transloco-loader';
-export { provideValidationMessages } from './validation-messages.provider';
-export { ValidationMessagesService } from './validation-messages.service';
-export { CustomMessage, FormMessageErrors, ValidateOptions } from './validation-messages.types';
+export * from '@aurora/components/validation-messages/public-api';
