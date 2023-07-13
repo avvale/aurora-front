9dd29975509f265241c5bcb01050e4275724f274
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-13 12:58:35 +0200

    minor fix

--- src/@aurora/aurora.provider.ts
+++ src/@aurora/aurora.provider.ts
@@ -1,16 +1,8 @@
 import { APP_INITIALIZER, EnvironmentProviders, Provider } from '@angular/core';
-import { AuroraGridManagerService, AuthenticationAuroraAdapterService, AuthenticationService, BootstrapService, COMPACT_NAVIGATION, DEFAULT_NAVIGATION, FUTURISTIC_NAVIGATION, GridManagerService, HORIZONTAL_NAVIGATION, IamAuroraAdapterService, IamService, SessionLocalStorageService, SessionService, UserMetaStorageLocalStorageAdapterService, UserMetaStorageService, compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation, provideApollo, provideValidationMessages, translocoLoader } from '@aurora';
-
-import './prototypes/string-to-camel-case.interface';
-import './prototypes/string-to-camel-case';
-import './prototypes/string-to-kebab-case.interface';
-import './prototypes/string-to-kebab-case';
-import './prototypes/string-to-pascal-case.interface';
-import './prototypes/string-to-pascal-case';
-import './prototypes/string-to-snake-case.interface';
-import './prototypes/string-to-snake-case';
-
+import { AuroraGridManagerService, AuthenticationAuroraAdapterService, AuthenticationDisabledAdapterGuard, AuthenticationMockAdapterService, AuthenticationService, BootstrapService, COMPACT_NAVIGATION, DEFAULT_NAVIGATION, EnvironmentsInformationMockAdapterService, EnvironmentsInformationService, FUTURISTIC_NAVIGATION, GridManagerService, HORIZONTAL_NAVIGATION, IamAuroraAdapterService, IamMockAdapterService, IamService, SessionLocalStorageService, SessionService, UserMetaStorageLocalStorageAdapterService, UserMetaStorageService, compactNavigation, defaultNavigation, futuristicNavigation, horizontalNavigation, provideApollo, provideValidationMessages } from '@aurora';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
 import { UserMetaStorageIamAdapterService } from 'app/modules/admin/apps/iam';
+import './aurora.prototypes';
 
 export const provideAurora = (): Array<Provider | EnvironmentProviders> =>
 {
