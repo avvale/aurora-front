309459c0f216da9fdf8d97b3bfdefd747b021dad
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-07-18 18:14:00 +0200

    refactor file upload component

--- src/@aurora/components/file-upload/file-upload.component.ts
+++ src/@aurora/components/file-upload/file-upload.component.ts
@@ -1,5 +1,7 @@
 import { NgForOf, NgIf } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { log } from '@aurora';
 import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry, NgxFileDropModule } from 'ngx-file-drop';
 
@@ -13,12 +15,13 @@ interface FileEntry
 }
 
 @Component({
-    selector   : 'au-file-upload',
-    templateUrl: './file-upload.component.html',
-    styleUrls  : ['./file-upload.component.scss'],
-    standalone : true,
-    imports    : [
-        NgForOf, NgIf, NgxFileDropModule,
+    selector       : 'au-file-upload',
+    templateUrl    : './file-upload.component.html',
+    styleUrls      : ['./file-upload.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    standalone     : true,
+    imports        : [
+        MatButtonModule, MatIconModule, NgForOf, NgIf, NgxFileDropModule,
     ],
 })
 export class FileUploadComponent
@@ -31,20 +34,14 @@ export class FileUploadComponent
     @Input('isMultiple') isMultiple: boolean = false;
     @Input('isDisabled') isDisabled: boolean = false;
     @Output('files') files = new EventEmitter<FileEntry[]>();
-
-    addedFiles: FileEntry[] = [];
-
-    constructor()
-    {
-        // Constructor
-    }
+    filesContainer: FileEntry[] = [];
 
     fileAdd(files: NgxFileDropEntry[]): void
     {
         // Reset list of files after each add ---> TODO: necessary? if multiple can we upload in multiple times?
-        this.addedFiles = [];
+        const newFiles: FileEntry[] = [];
 
-        for (const droppedFile of files)
+        for (const [index, droppedFile] of files.entries())
         {
             // Is it a file or a directory?
             if (droppedFile.fileEntry.isFile)
@@ -52,25 +49,29 @@ export class FileUploadComponent
                 // It was a file --> ADD TO FILES
                 const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
                 const fileName = droppedFile.relativePath;
-                let fileType = '';
-                let fileSize = 0;
 
                 log('[DEBUG] File relative path file uploaded: ', droppedFile.relativePath);
 
-                fileEntry.file((file: File) =>
-                {
-                    log('[DEBUG] Properties file uploaded: ', file);
-                    fileType = file.type;
-                    fileSize = file.size;
-                });
+                fileEntry
+                    .file((file: File) =>
+                    {
+                        log('[DEBUG] Properties file uploaded: ', file);
+                        newFiles.push({
+                            file  : droppedFile,
+                            fsFile: fileEntry,
+                            name  : fileName,
+                            type  : file.type,
+                            size  : file.size,
+                        });
 
-                this.addedFiles.push({
-                    file  : droppedFile,
-                    fsFile: fileEntry,
-                    name  : fileName,
-                    type  : fileType,
-                    size  : fileSize,
-                });
+                        if (index === files.length - 1)
+                        {
+                            this.filesContainer = [...this.filesContainer, ...newFiles];
+
+                            // After adding all, emit files
+                            this.files.emit(this.filesContainer);
+                        }
+                    });
             }
             else
             {
@@ -80,25 +81,22 @@ export class FileUploadComponent
                 log('[DEBUG] Uploaded a directory: ', fileEntry);
             }
         }
-
-        // After adding all, emit files
-        this.files.emit(this.addedFiles);
     }
 
     fileEnter(event): void
     {
-        console.log(event);
+        log('[DEBUG] onFileOver event: ', event);
     }
 
     fileLeave(event): void
     {
-        console.log(event);
+        log('[DEBUG] onFileLeave event: ', event);
     }
 
     fileRemove(file): void
     {
-        this.addedFiles = this.addedFiles.filter(f => f.name != file.name);
-        this.files.emit(this.addedFiles);
+        this.filesContainer = this.filesContainer.filter(f => f.name != file.name);
+        this.files.emit(this.filesContainer);
     }
 
     calculateSize(size: number): { size: number; unit: string; }
