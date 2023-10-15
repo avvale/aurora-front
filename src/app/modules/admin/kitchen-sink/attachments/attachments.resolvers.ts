13c8b29bec8cdb3cb9b09736580d780c3008e0c2
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2023-10-15 23:28:59 +0200

    get attachment families

--- src/app/modules/admin/kitchen-sink/attachments/attachments.resolvers.ts
+++ src/app/modules/admin/kitchen-sink/attachments/attachments.resolvers.ts
@@ -1,16 +1,32 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
-import { Action, ActionService } from '@aurora';
+import { AttachmentFamilyService } from '@apps/common/attachment-family';
+import { CommonAttachmentFamily } from '@apps/common/common.types';
+import { ActionService } from '@aurora';
 
-export const attachmentsViewResolver: ResolveFn<Action> = (
+export const attachmentsViewResolver: ResolveFn<{
+    objects: CommonAttachmentFamily[];
+}> = (
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
 ) =>
 {
     const actionService = inject(ActionService);
+    const attachmentFamilyService = inject(AttachmentFamilyService);
 
-    return actionService.action({
+    actionService.action({
         id          : 'kitchenSinh::attachments.detail.view',
         isViewAction: true,
+    });
+
+    return attachmentFamilyService.get({
+        query: {
+            include: [
+                { association: 'resource' },
+            ],
+            where: {
+                '$resource.code$': 'kitchen-sink',
+            },
+        },
     });
 };
