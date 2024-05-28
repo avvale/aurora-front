813f67dba7e531f9a250af6a672a0b63238e0437
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2024-05-28 14:11:11 +0200

    update settings

--- src/app/modules/admin/apps/settings/security/security.component.ts
+++ src/app/modules/admin/apps/settings/security/security.component.ts
@@ -1,11 +1,14 @@
+import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { SnackBarInvalidFormComponent, ViewDetailComponent, log } from '@aurora';
 import { TranslocoModule } from '@ngneat/transloco';
+import { RxwebValidators } from '@rxweb/reactive-form-validators';
 
 @Component({
     selector       : 'settings-security',
@@ -14,36 +17,77 @@ import { TranslocoModule } from '@ngneat/transloco';
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone     : true,
     imports        : [
-        FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule,
+        AsyncPipe, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatIconModule,
         MatInputModule, MatSlideToggleModule, MatButtonModule, TranslocoModule,
     ],
 })
-export class SettingsSecurityComponent implements OnInit
+export class SettingsSecurityComponent extends ViewDetailComponent
 {
-    securityForm: UntypedFormGroup;
-
-    /**
-     * Constructor
-     */
-    constructor(
-        private _formBuilder: UntypedFormBuilder,
-    )
+    onSubmit($event): void
     {
+        // we have two nested forms, we check that the submit comes from the button
+        // that corresponds to the main form to the main form
+        if ($event.submitter.getAttribute('form') !== $event.submitter.form.getAttribute('id'))
+        {
+            $event.preventDefault();
+            $event.stopPropagation();
+            return;
+        }
+
+        // force validate repeat password, usually only validated if the value of the input changes.
+        // in this case depends on the change of the password field
+        this.fg.get('user.repeatPassword')?.updateValueAndValidity();
+
+        // manage validations before execute actions
+        if (this.fg.invalid)
+        {
+            log('[DEBUG] Error to validate form: ', this.fg);
+            this.validationMessagesService.validate();
+
+            this.snackBar.openFromComponent(
+                SnackBarInvalidFormComponent,
+                {
+                    data: {
+                        message   : `${this.translocoService.translate('InvalidForm')}`,
+                        textButton: `${this.translocoService.translate('InvalidFormOk')}`,
+                    },
+                    panelClass      : 'error-snackbar',
+                    verticalPosition: 'top',
+                    duration        : 10000,
+                },
+            );
+            return;
+        }
+
+        /* // remove fields to update or create account
+        this.user.removeControl('repeatPassword');
+
+        this.actionService.action({
+            id: mapActions(
+                this.currentViewAction.id,
+                {
+                    'iam::account.detail.new' : 'iam::account.detail.create',
+                    'iam::account.detail.edit': 'iam::account.detail.update',
+                },
+            ),
+            isViewAction: false,
+        }); */
     }
 
-    // -----------------------------------------------------------------------------------------------------
-    // @ Lifecycle hooks
-    // -----------------------------------------------------------------------------------------------------
-
-    /**
-     * On init
-     */
-    ngOnInit(): void
+    createForm(): void
     {
         // Create the form
-        this.securityForm = this._formBuilder.group({
-            currentPassword  : [''],
-            newPassword      : [''],
+        this.fg = this.fb.group({
+            currentPassword: ['', [Validators.required]],
+            newPassword    : ['', [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(30),
+                RxwebValidators.password({
+                    validation: { digit: true, specialCharacter: true, lowerCase: true, upperCase: true },
+                    message   : { digit: 'PasswordDigit', specialCharacter: 'PasswordSpecialCharacter', lowerCase: 'PasswordLowerCase', upperCase: 'PasswordUpperCase' },
+                }),
+            ]],
             twoStep          : [true],
             askPasswordChange: [false],
         });
