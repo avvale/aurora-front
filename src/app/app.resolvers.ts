68545504cbea432a57bb86bb6aa9d8987f95bfc1
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2025-04-29 18:01:16 +0200

    add initializer service and minor fix

--- src/app/app.resolvers.ts
+++ src/app/app.resolvers.ts
@@ -3,13 +3,15 @@ import { NavigationService } from 'app/core/navigation/navigation.service';
 import { NotificationsService } from 'app/layout/common/notifications/notifications.service';
 import { QuickChatService } from 'app/layout/common/quick-chat/quick-chat.service';
 import { ShortcutsService } from 'app/layout/common/shortcuts/shortcuts.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, from, switchMap } from 'rxjs';
 
 // ---- customizations ----
 import { InboxService } from '@apps/message/inbox';
-import { GridTranslationsTranslocoInitService } from '@aurora';
+import { GridTranslationsTranslocoInitService, InitializerService } from '@aurora';
 
-export const initialDataResolver = () => {
+export const initialDataResolver = () =>
+{
+    const initializerService = inject(InitializerService);
     const navigationService = inject(NavigationService);
     const notificationsService = inject(NotificationsService);
     const quickChatService = inject(QuickChatService);
@@ -17,16 +19,17 @@ export const initialDataResolver = () => {
 
     // ---- customizations ----
     const inboxService = inject(InboxService);
-    // prettier-ignore
     const gridTranslationsTranslocoInitService = inject(GridTranslationsTranslocoInitService);
 
-    // Fork join multiple API endpoint calls to wait all of them to finish
-    return forkJoin([
-        navigationService.get(),
-        notificationsService.getAll(),
-        quickChatService.getChats(),
-        shortcutsService.getAll(),
-        inboxService.checkMessagesInbox(),
-        gridTranslationsTranslocoInitService.getAll(),
-    ]);
+    return from(initializerService.resolverInitializer())
+        .pipe(
+            switchMap(() => forkJoin([
+                navigationService.get(),
+                notificationsService.getAll(),
+                quickChatService.getChats(),
+                shortcutsService.getAll(),
+                inboxService.checkMessagesInbox(),
+                gridTranslationsTranslocoInitService.getAll(),
+            ])),
+        );
 };
