df6f73a9c5009d620158baca0a64dcc13c171ff6
José Carlos Rodríguez Palacín <carlos.rodriguez.palacin@gmail.com>
2026-02-09 16:17:52 +0100

    feat(styles): add phone number format styles and update import path in main.scss

--- src/app/modules/admin/admin.navigation.ts
+++ src/app/modules/admin/admin.navigation.ts
@@ -1,4 +1,5 @@
 import { auditingNavigation } from '@apps/auditing/auditing.navigation';
+import { businessPartnerPortalNavigation } from '@apps/business-partner-portal/business-partner-portal.navigation';
 import { commonNavigation } from '@apps/common/common.navigation';
 import { iamNavigation } from '@apps/iam/iam.navigation';
 import { messageNavigation } from '@apps/message/message.navigation';
@@ -8,7 +9,6 @@ import { searchEngineNavigation } from '@apps/search-engine/search-engine.naviga
 import { supportNavigation } from '@apps/support/support.navigation';
 import { toolsNavigation } from '@apps/tools/tools.navigation';
 import { FuseNavigationItem } from '@fuse/components/navigation';
-import { businessPartnerPortalNavigation } from './apps/business-partner-portal/business-partner-portal.navigation';
 import { kitchenSinkNavigation } from './kitchen-sink/kitchen-sink.navigation';
 
 export const adminNavigation: FuseNavigationItem[] = [
